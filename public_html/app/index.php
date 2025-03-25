2e31cb3dbbbc13ca022e33f8d876533742250bb0
seno21 <adarafaranisa443@gmail.com>
2025-03-25 11:50:56 +0700

    first commit

---
+++ public_html/app/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to App</title>
+</head>
+
+<body>
+    <h1>Hallo ini App Index WEB</h1>
+    <p style="color: #0f0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, inventore ex! Dolore laboriosam iste voluptatem eligendi. Est expedita laudantium ullam distinctio iste quo ex eveniet voluptatem. Natus minus vitae tempora.</p>
+    <?php
+    echo "Halo Dunia";
+    ?>
+</body>
+
+</html>
---
+++ public_html/portal/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Portal</title>
+</head>
+
+<body>
+    <h1>Hallo ini Portal Index WEB</h1>
+    <p style="color: #f00">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, inventore ex! Dolore laboriosam iste voluptatem eligendi. Est expedita laudantium ullam distinctio iste quo ex eveniet voluptatem. Natus minus vitae tempora.</p>
+    <?php
+    echo "Halo Dunia";
+    ?>
+</body>
+
+</html>
